d46cccba3c75dcc64a169ff5c24d18067f77fa85
trungfpt594 <nguyenchitrung800@gmail.com>
2025-05-29 22:20:21 +0700

    Add files via upload

---
+++ FirstDegree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+void fist_degree_solution(double a, double b)
+{
+    if (a == 0 && b == 0)
+    {
+        Console.WriteLine("vo so nghiem");
+    }
+    else if (a == 0 && b == 0)
+    {
+        Console.WriteLine("Vo nghiem");
+    }
+    else
+    {
+        Console.WriteLine("X={0}", -b / a);
+    }
+}
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Phuong trinh bac 1: ax+b=0");
+Console.WriteLine("nhap he so a:");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("nhap he so b:");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("{0}x+{1}=0", a, b);
+fist_degree_solution(a, b);
+Console.WriteLine();
---
+++ FirstProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Xin chào các bạn!");
+Console.WriteLine("Tôi là SE 1848");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Tôi hứa là tôi sẽ học nghiêm túc");
+Console.ForegroundColor= ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine("Tôi hứa là tôi sẽ làm được!");
+Console.ReadLine();
